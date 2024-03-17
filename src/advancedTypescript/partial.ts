10e25a93b1fec262412f173e4f6b801389804545
root <root@Unaislaptop>
2024-03-17 12:57:37 +0530

    added partial

---
+++ src/advancedTypescript/partial.ts
@@ -0,0 +1,21 @@
+//--------------------------------------------PARTIAL----------------------------------------------------------
+
+//Partial makes all properties of a type optional , creating a type with the same properties, but each marked as optional
+
+
+// specificaly when you want to update
+
+interface Users {
+    id : string
+    name : string,
+    email : string,
+    age : number,
+    password : string
+}
+
+type updatedUsers = Pick<User, 'name' | 'email' | 'age'>
+type updatePropsOptional = Partial<updatedUsers> // this makes everything optional
+
+function updateUser(updatedProps : updatePropsOptional){
+    // hit the database to update user
+}
